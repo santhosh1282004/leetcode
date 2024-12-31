257bf9d58e1e323bf8135a0af82df51f823c0d06
ss.santhosh <147931093+santhosh1282004@users.noreply.github.com>
2024-12-31 22:08:34 +0530

    adding commend in  Single Number.136.py

--- Single Number.136.py
+++ Single Number.136.py
@@ -1,6 +1,12 @@
 nums = [1,3,4,5,3,5,1,2,5]
+
+#it remove all duplicate valus
 set=set(nums)
+
+#using this loop remove the all number ones
 for i in set:
+    # in array remove all elwment in set
     nums.remove(i)
+    # if the number have onle one in the array then it removed by (9 line) so it is ot in array so print the number  
     if not(i in nums):
         print(i)
