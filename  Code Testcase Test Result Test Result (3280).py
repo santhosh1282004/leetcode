33a9f89339231e2b1f6dd6d2b02c856e066880c0
ss.santhosh <147931093+santhosh1282004@users.noreply.github.com>
2025-01-01 21:47:05 +0530

    improving time and space complicity

---  Code Testcase Test Result Test Result (3280).py
+++
@@ -1,7 +0,0 @@
-date="2080-02-29"
-two = []
-for i in date.split('-'):
-    i = bin(int(i))[2:]
-    two.append(i)
-a='-'.join(two)
-print(a)
---
+++ Code Testcase Test Result Test Result (3280).py
@@ -0,0 +1,2 @@
+date="2080-02-29"
+print(bin(int(date[0:4]))[2:]+"-"+bin(int(date[5:7]))[2:]+"-"+bin(int(date[8:10]))[2:])
