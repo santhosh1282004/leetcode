40f78dc6c741ca211e3dd76af22f6a31f80ba4bc
ss.santhosh <147931093+santhosh1282004@users.noreply.github.com>
2025-01-08 20:55:45 +0530

    Update Count Prefix and Suffix Pairs I 3042.java

--- Count Prefix and Suffix Pairs I 3042.java
+++ Count Prefix and Suffix Pairs I 3042.java
@@ -1,19 +1,18 @@
 public class leetcode {
 	public static void main(String []args) {
 		String [] words= {"a","aba","ababa","aa"};
-		int len_array=words.length;
-        int len_str;
-        int len_str1;
+	int len_array=words.length;
         int count=0;
         for(int i=0;i<len_array;i++){
-            len_str= words[i].length();
+            String word1= words[i];
             for(int j=i+1;j<len_array;j++){
-                len_str1=words[j].length();                
-                if(len_str<=len_str1 && words[i].equals(words[j].substring(0,len_str)) && words[i].equals(words[j].substring(len_str1-len_str,len_str1))){
-                	count++;
+               String word2=words[j];
+                if(word2.startsWith(words[i]) && word2.endsWith(words[i])){
+                        count++;
+                    }
                 }
             }
-        }
+
         System.out.print(count);
 	}
 }
