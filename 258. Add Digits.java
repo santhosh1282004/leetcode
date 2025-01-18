46d1442875918481848e1afe837b5002632e5835
ss.santhosh <147931093+santhosh1282004@users.noreply.github.com>
2025-01-18 22:15:15 +0530

    Update 258. Add Digits.java

--- 258. Add Digits.java
+++ 258. Add Digits.java
@@ -1,14 +1,6 @@
 class Solution {
     public int addDigits(int num) {
-        while(num>9){
-            int sum=0;
-            while(num>0){
-                sum+=num%10;
-                num/=10;
-            }
-            num=sum;
-        }
-        return num;
-        
+        int a=1+(num-1)%9;
+        return a; 
     }
 }
