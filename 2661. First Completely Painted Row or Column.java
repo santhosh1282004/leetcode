4ecef82a537d6ad041b68b0e7d5c845e090892ff
ss.santhosh <147931093+santhosh1282004@users.noreply.github.com>
2025-01-20 21:40:25 +0530

    Update 2661. First Completely Painted Row or Column.java

--- 2661. First Completely Painted Row or Column.java
+++ 2661. First Completely Painted Row or Column.java
@@ -2,25 +2,25 @@ class solution {
     public static void main(String[] args) {
         int[] arr={1,4,5,2,6,3};
         int[][] mat={{4,3,5},{1,2,6}};
-        int arr_len=arr.length;
-        int mat_collem_len=mat.length;
-        int mat_row_len=mat[0].length;
-        int [] collem= new int[mat_collem_len];
-        int [] row= new int[mat_row_len];
-        int r=0;
-        for(int i=0;i<arr_len;i++){
-            for(int j=0;j<mat_collem_len;j++){
-                for(int k=0;k<mat_row_len;k++){
-                    if(arr[i]==mat[j][k]){
-                        collem[j]++;
-                        row[k]++;
-                        if(collem[j]==mat_row_len || row[k]== mat_collem_len){
-                            r=i;
-                            break;
-                        }
-                    }
-                }
+        int[] map = new int[arr.length + 1];
+        for (int i = 0; i < arr.length; i++) {
+            map[arr[i]] = i;
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < mat.length; i++) {
+            int max = 0;
+            for (int j = 0; j < mat[i].length; j++) {
+                max = Math.max(max, map[mat[i][j]]);
             }
-        }return r;
+            ans = Math.min(ans, max);
+        }
+        for (int i = 0; i < mat[0].length; i++) {
+            int max = 0;
+            for (int j = 0; j < mat.length; j++) {
+                max = Math.max(max, map[mat[j][i]]);
+            }
+            ans = Math.min(ans, max);
+        }
+        System.out.println(ans);
     }
 } 
