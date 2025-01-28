ecc8d0108e4d169344b384d0bb7c7b6f99be4d30
ss.santhosh <147931093+santhosh1282004@users.noreply.github.com>
2025-01-28 21:24:36 +0530

    Update 709. To Lower Case.java

--- 709. To Lower Case.java
+++ 709. To Lower Case.java
@@ -1,15 +1,6 @@
 class Solution {
     public String toLowerCase(String s) {
-        String str="";
-        for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(c>64 && c<91){
-                str+=(char)(s.charAt(i)+32);
-            }else{
-                str+=c;
-            }
-            
-        }
-        return str;
+        return s.toLowerCase();
+        
     }
 }
