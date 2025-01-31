e8568936cbb82c59c5d8c025f7e1b8adc7f9b176
ss.santhosh <147931093+santhosh1282004@users.noreply.github.com>
2025-01-31 22:25:17 +0530

    Update 1768. Merge Strings Alternately.java

--- 1768. Merge Strings Alternately.java
+++ 1768. Merge Strings Alternately.java
@@ -1,20 +1,18 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int len1=word1.length();
-        int len2=word2.length();
         int i=0;
-        int j=0;
         String str="";
-        while(i<len1 || j<len2){
-            if(i<len1){
-                str+=word1.charAt(i);
-                i++;
-            }
-            if(j<len2){
-                str+=word2.charAt(j);
-                j++;
-            }
+        while(i<word1.length() && i<word2.length()){
+            str+=word1.charAt(i);
+            str+=word2.charAt(i);
+            i++;
         }
-        return str;
+        if(i<word1.length()){
+            str+=word1.substring(i,word1.length());
+        }
+        else{
+            str+=word2.substring(i,word2.length());
+        }
+        return str.toString();
     }
 }
