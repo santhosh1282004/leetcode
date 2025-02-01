510c8283ad1e44d7f8043a5d4ab70312e11a95b7
ss.santhosh <147931093+santhosh1282004@users.noreply.github.com>
2025-02-01 21:48:32 +0530

    Update 3151. Special Array I.java

--- 3151. Special Array I.java
+++ 3151. Special Array I.java
@@ -1,10 +1,7 @@
 class Solution {
     public boolean isArraySpecial(int[] nums) {
-        int len=nums.length;
-        for(int i=0;i<len-1;i++){
-            if(((nums[i]+nums[i+1])&1)==0){
-                return false;
-            }
+        for(int i=0;i<nums.length-1;i++){
+            if(((nums[i]+nums[i+1])&1)==0)return false;
         }
         return true;
     }
