aac9fff550037981ad8e9221b0f7405cc9ccb713
ss.santhosh <147931093+santhosh1282004@users.noreply.github.com>
2025-02-07 21:30:55 +0530

    Update 1539. Kth Missing Positive Number.java

--- 1539. Kth Missing Positive Number.java
+++ 1539. Kth Missing Positive Number.java
@@ -1,14 +1,11 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        int len=arr.length;
-        if(k<arr[0]){
-            return k;
-        }
-         for(int i=0;i<len;i++){
-            if((arr[i]-(i+1))>=k){
-                return arr[i-1]+k-(arr[i-1]-i);
+        for(int i:arr){
+            if(i>k){
+                return k;
             }
-         }
-         return (arr[len-1]+k-(arr[len-1]-len));
+            k++;
+        }
+        return k;
     }
 }
