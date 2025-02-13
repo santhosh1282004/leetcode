3220ade15157458f043074de7773257e67b410eb
ss.santhosh <147931093+santhosh1282004@users.noreply.github.com>
2025-02-13 21:03:24 +0530

    Create 6. Zigzag Conversion.java

---
+++ 6. Zigzag Conversion.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows==1)return s;
+        String str="";
+        int x=(numRows-1)*2;
+        int y=0;
+        for(int i=0;i<numRows;i++){
+            int j=i;
+            boolean b=true;
+            while(j<s.length()){
+                str=str+s.charAt(j);
+                if(y==0 || x==0){
+                    j+=(numRows-1)*2;
+                }
+                else if(b){
+                    j+=x;
+                    b=false;
+                }
+                else{
+                    j+=y;
+                    b=true;
+                }
+            }
+            x-=2;
+            y+=2;
+        }
+        return str;
+    }
+}
