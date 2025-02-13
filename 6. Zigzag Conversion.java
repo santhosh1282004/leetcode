5f1ac64152ece108a3d0631587c42b446b8d3694
ss.santhosh <147931093+santhosh1282004@users.noreply.github.com>
2025-02-13 21:06:09 +0530

    Update 6. Zigzag Conversion.java

--- 6. Zigzag Conversion.java
+++ 6. Zigzag Conversion.java
@@ -2,17 +2,15 @@ class Solution {
     public String convert(String s, int numRows) {
         int len=s.length();
         if(numRows==1 || numRows >= len)return s;
-         StringBuilder str=new StringBuilder(len);
+        StringBuilder str=new StringBuilder(len);
         int x=(numRows-1)*2;
         for(int i=0;i<numRows;i++){
-            int j=i;
-            while(j<len){
+            for(int j=i;j<len;j+=x){
                 str.append(s.charAt(j));
                 int y=x+j-(i*2);
                 if(i>0 && i<numRows-1 && y<len){
                     str.append(s.charAt(y));
                 }
-                j+=x;
             }
         }
         return str.toString();
