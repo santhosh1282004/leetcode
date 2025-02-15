fd03c4a53dde2bc388743e27cf2b2cc3736e02bd
ss.santhosh <147931093+santhosh1282004@users.noreply.github.com>
2025-02-15 21:20:24 +0530

    Update 268. Missing Number.java

--- 268. Missing Number.java
+++ 268. Missing Number.java
@@ -2,8 +2,8 @@ class Solution {
     public int missingNumber(int[] nums) {
         int len=nums.length;
         int rem= (len*(len+1))/2;
-        for(int i=0;i<len;i++){
-            rem-=nums[i];
+        for(int i:nums){
+            rem-=i;
         }
         return rem;
     }
